8c201bb2de6434f6f1c59e704b91a8c95cd9dce9
JOAQUIM FONTES MATOS <joaquimatos50@gmail.com>
2024-06-15 20:58:00 +0100

    remove

--- anime_list_manager_frontend/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import{routes} from './app.routes'
-
-export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(HttpClientModule), provideRouter(routes)],
-};
