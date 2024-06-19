d0f909aae6284028a8fa7da99d4a796802e5d6ad
JOAQUIM FONTES MATOS <joaquimatos50@gmail.com>
2024-06-19 18:42:23 +0100

    added some very nice things

---
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.min(8),
+  ]);
+
+  login() {}
+  register(){}
+}
