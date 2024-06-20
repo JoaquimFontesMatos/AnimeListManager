f8c2d1a3dc1660ec63307d3b252bf28675e75e4b
JoaquimFontesMatos <joaquimatos50@gmail.com>
2024-06-20 16:32:59 +0100

    Update landing-page.component.ts

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
-
+  constructor(private router: Router) {
+    this.router.navigate(['/auth/login/']);
+  }
 }
